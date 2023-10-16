3daf3b50c83ddd041f7bee53e92fe612d82b730d
JAmelang01 <jamelang@atu.edu>
2023-10-16 09:51:30 -0500

    Tried to Add Jump
    Tried to add a jump to the sprite.  Still needs to work on it.

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 5.0f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.2f;
+
+    private bool isGrounded;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -17,3 +17,4 @@ public class Movement : MonoBehaviour
         transform.Translate(movement);
     }
 }
+

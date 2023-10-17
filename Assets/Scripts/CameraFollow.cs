a8d49d03c0b936556afe4af2e8317a7ed299c466
WilsonAEscobar <wilsonescobar909@outlook.com>
2023-10-17 18:01:50 -0500

    Added Assets and Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed = 2f; // speed that the camera will follow at.
+    public float yOffset =1f; //
+    public Transform target;
+
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y+yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
+    }
+}

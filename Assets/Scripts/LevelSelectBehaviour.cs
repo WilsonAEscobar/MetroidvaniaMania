5d2d592c929d87c9ed2e28388e358419ba23ad64
WilsonAEscobar <wilsonescobar909@outlook.com>
2023-10-16 21:16:34 -0500

    buttons work
    buttons work

--- Assets/Scripts/LevelSelectBehaviour.cs
+++ Assets/Scripts/LevelSelectBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class LevelSelectBehaviour : MonoBehaviour
@@ -7,10 +8,26 @@ public class LevelSelectBehaviour : MonoBehaviour
     public GameObject[] levelPanels;
 
     // Start is called before the first frame update
+    public void backToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
     public void showLevelPanel(int levelNum)
     {
         levelPanels[levelNum].SetActive(true);
     }
+
+    public void startLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void closePanel(int levelNum)
+    {
+        levelPanels[levelNum].SetActive(false);
+    }
+    
+    
 
     
 }
